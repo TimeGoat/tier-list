4a76439ad6f7c35d30ca73157628dac95474b50b
TimeGoat <41596884+TimeGoat@users.noreply.github.com>
2023-04-21 09:48:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 
 
-// Select all elements with the class 'toggle-paragraph' and store them in the 'headings' array
-const headings = document.querySelectorAll('.toggle-paragraph');
-
-// For each element in the 'headings' array, add a click event listener
-headings.forEach(heading => {
-  // When clicked, select the next element sibling of the clicked heading and store it in the 'paragraph' variable
-  const paragraph = heading.nextElementSibling;
-  // Select the parent element of the clicked heading and store it in the 'row' variable
-  const row = heading.parentElement;
-  // Toggle the 'show' class on the 'row' element
-  row.classList.toggle('show');
-});
-
-
